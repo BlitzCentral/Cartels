001ecb6585ca6e733e48321fdd27d332de2f2abd
BLitZzMc <michael@spigotcodingacademy.co.uk>
2018-06-29 08:39:22 +0100

    Started CartelChat code.

--- src/main/java/com/spigotcodingacademy/deviousminescartel/listeners/PlayerChatListener.java
+++ src/main/java/com/spigotcodingacademy/deviousminescartel/listeners/PlayerChatListener.java
@@ -1,6 +1,7 @@
 package com.spigotcodingacademy.deviousminescartel.listeners;
 
 import com.spigotcodingacademy.deviousminescartel.DeviousMines;
+import com.spigotcodingacademy.deviousminescartel.manager.PlayerData;
 import com.spigotcodingacademy.deviousminescartel.utils.Chat;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -26,18 +27,27 @@ public class PlayerChatListener implements Listener {
         String pn = player.getDisplayName();
 
         String prefix = getPrefixSuffix(player, "prefix");
-        String suffix = getPrefixSuffix(player, "suffix");
-
-        if (!DeviousMines.getCartelManager().inCartel(player)) {
-            event.setFormat(Chat.color( prefix + " &f" + pn + " &8&l>> &f" + event.getMessage()));
-            return;
-        }
 
         File cartel = new File(DeviousMines.getInstance().getDataFolder() + "/data/players", player.getUniqueId().toString() + ".yml");
         YamlConfiguration CartelData = YamlConfiguration.loadConfiguration(cartel);
-
         String cartelName = CartelData.getString("Player.Cartel");
 
-        event.setFormat(Chat.color("&8[&6" + cartelName + "&8] " + prefix + " &f" + pn + " &8&l>> &f" + event.getMessage()));
+        if (PlayerData.cartelChat.containsKey(player)) {
+            event.setCancelled(true);
+
+            event.setFormat(Chat.color( "&8[&bCartels&8]" + " &8[&b" + cartelName + "&8] &f" + pn + "&8&l>> &f" + event.getMessage()));
+
+            return;
+        }
+
+        if (!PlayerData.cartelChat.containsKey(player)) {
+
+            if (!DeviousMines.getCartelManager().inCartel(player)) {
+                event.setFormat(Chat.color( prefix + " &f" + pn + " &8&l>> &f" + event.getMessage()));
+                return;
+            }
+
+            event.setFormat(Chat.color("&8[&6" + cartelName + "&8] " + prefix + " &f" + pn + " &8&l>> &f" + event.getMessage()));
+        }
     }
 }
